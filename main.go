46812d1be36842c098d6019b0f0d0779819b4665
Mritunjay Paswan <mritunjay@manthanitsolutions.in>
2024-10-14 01:25:01 +0530

    - Crud completed

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand/v2"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -34,23 +36,84 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", rootRoute).Methods("GET")
 	r.HandleFunc("/get-movies", getAllMovies).Methods("GET")
+	r.HandleFunc("/get-movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/add-movie", addMovie).Methods("POST")
+	r.HandleFunc("/delete-move/{id}", deleteMovie).Methods("DELETE")
+	r.HandleFunc("/update-movie/{id}", updateMovie).Methods("PUT")
 
 	fmt.Printf("Server is running on port 8000\n")
 	log.Fatal(http.ListenAndServe(":8002", r))
 }
 
-func getAllMovies(w http.ResponseWriter, r *http.Request) {
-	moviesList := movies // This retrieves the list of movies
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	movieId := mux.Vars(r)
+	var movie Movie
 
-	// Prepare the response structure
-	response := map[string]interface{}{
-		"success": true,
-		"data":    moviesList, // This includes the movie list under 'data'
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	for index, val := range movies {
+		if val.ID == movieId["id"] {
+			movies[index].Director = movie.Director
+			movies[index].Title = movie.Title
+			movies[index].Director = movie.Director
+		}
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"success": true,
+			"data":    movies[index],
+		})
+		return
 	}
 
-	// Set content type to JSON
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	// Encode and send the JSON response
+	params := mux.Vars(r)
+	movieId := params["id"]
+	for index, movie := range movies {
+		if movie.ID == movieId {
+			movies = append(movies[:index], movies[index+1:]...)
+			json.NewEncoder(w).Encode(map[string]interface{}{"success": true, "message": "Movie deleted successfully."})
+		}
+	}
+
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	movieId := params["id"]
+	for _, movie := range movies {
+		if movie.ID == movieId {
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
+	}
+}
+
+func addMovie(w http.ResponseWriter, r *http.Request) {
+	var movie Movie
+
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+
+	movie.ID = strconv.Itoa(rand.IntN(1000000))
+	movies = append(movies, movie)
+
+	res := Response{
+		Success: true,
+		Data:    movies,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(res)
+}
+
+func getAllMovies(w http.ResponseWriter, r *http.Request) {
+	moviesList := movies
+	response := map[string]interface{}{
+		"success": true,
+		"data":    moviesList,
+	}
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(response)
 }
 
